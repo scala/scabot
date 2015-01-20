904a7952173dd5fd05e5a56ba073f9968d4c54f5
Adriaan Moors <adriaan.moors@typesafe.com>
2015-01-19 20:18:22 -0500

    Build logic

--- server/src/main/scala/Actors.scala
+++ server/src/main/scala/Actors.scala
@@ -1,6 +1,8 @@
 package scabot
 package server
 
+import java.util.NoSuchElementException
+
 import akka.actor._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -20,7 +22,7 @@ trait Actors {
   // pull requst actors are supervised by their project actor
   private def projectActorName(user: String, repo: String) = s"$user-$repo"
 
-  def tellProjectActor(user: String, repo: String, msg: ProjectMessage) =
+  def tellProjectActor(user: String, repo: String)(msg: ProjectMessage) =
     system.actorSelection(githubActor.path / projectActorName(user, repo)) ! msg
 
 
@@ -45,6 +47,7 @@ trait Actors {
 
   case object Synch extends ProjectMessage
 
+  // represents a github project at github.com/${config.github.user}/${config.github.repo}
   class ProjectActor(config: Config) extends Actor with ActorLogging {
     lazy val githubApi = new GithubConnection(config.github)
     import context._
@@ -62,7 +65,7 @@ trait Actors {
         // TODO make timeout configurable
         context.system.scheduler.scheduleOnce(30 minutes, self, Synch) 
 
-      case ev@PullRequestEvent(action, nb, pull_request) =>
+      case ev@PullRequestEvent(_, nb, _) =>
         prActor(nb) ! ev
 
       case PullRequestReviewCommentEvent("created", pull_request, comment, _) =>
@@ -72,10 +75,10 @@ trait Actors {
         prActor(issue.number) ! comment
 
       case js@JobState(_, _, bs) =>
-        for {
+        (for {
           prParam <- Try(bs.parameters(PARAM_PR)) // we only care about jobs we started, and which thus have this parameter (when restarted manually, they should be carried forward automatically)
-          prNum <- Try(prParam.toInt)
-        } prActor(prNum) ! js
+          prNum   <- Try(prParam.toInt)
+        } prActor(prNum) ! js) // TODO: report failure
     }
   }
 
@@ -121,8 +124,8 @@ trait Actors {
     private def handlePR(action: String, pull: PullRequest) = {
       checkMilestone(pull)
       checkLGTM(pull)
-      combineSuccesses(pull)
-      buildCommitsIfNeeded(pull) // TODO: distinguish synchronize/open action
+      propagateEarlierStati(pull)
+      buildCommitsIfNeeded(pull)
       //      execCommands(pull)
     }
 
@@ -147,15 +150,16 @@ trait Actors {
     }
 
 
-    private def launchBuild(sha: String) =
-      jenkinsApi.buildJob(config.jenkins.job, Map(
-        PARAM_PR -> pr.toString,
+    private def launchBuild(sha: String): Future[String] = {
+      val fut = jenkinsApi.buildJob(config.jenkins.job, Map(
+        PARAM_PR        -> pr.toString,
         PARAM_REPO_USER -> config.github.user,
         PARAM_REPO_NAME -> config.github.repo,
-        PARAM_REPO_REF -> sha
-      )) onFailure {
-        case e => log.info(s"launchBuild($sha) failed: $e");
-      }
+        PARAM_REPO_REF  -> sha
+      ))
+      fut onFailure { case e =>  log.info(s"launchBuild($sha) failed: $e") }
+      fut
+    }
 
     def milestoneForBranch(branch: String): Future[Milestone] = for {
       mss <- githubApi.repoMilestones()
@@ -179,47 +183,96 @@ trait Actors {
     // purposefully only at start of line to avoid conditional LGTMs
       hasLGTM <- githubApi.pullRequestComments(pr).map(_.exists(_.body.startsWith("LGTM")))
       hasReviewedLabel <- hasLabelNamed("reviewed")
-    } yield {
+    } yield { // TODO react to labeled/unlabeled event on webhhook
       if (hasLGTM) { if (!hasReviewedLabel) githubApi.addLabel(pr, List(Label("reviewed"))) }
       else if (hasReviewedLabel) githubApi.deleteLabel(pr, "reviewed")
     }
 
-    // add failing status in context "combined" if commits before the last one failed (so that the merge button is green iff all commits are successes)
-    private def combineSuccesses(pull: PullRequest) = for {
-      commits       <- githubApi.pullRequestCommits(pr)
-      lastStatus    <- githubApi.commitStatus(commits.last.sha)
-      earlierStati  <- Future.sequence(commits.init.map(c => githubApi.commitStatus(c.sha)))
-      failingCommits = earlierStati.collect{ case c if c.state != "success" => (c.sha, c.state) }
-    } yield {
-      if (failingCommits.isEmpty) {
-        if (lastStatus.state != "success")
-          githubApi.postStatus(commits.last.sha, CommitStatus("success", Some("combined"), Some("All previous commits successful.")))
-      } else if (lastStatus.state == "success") {
-        val worstState = if (failingCommits.map(_._2).contains("failure")) "failure" else "pending"
-        githubApi.postStatus(commits.last.sha, CommitStatus(worstState, Some("combined"), Some(failingCommits.mkString)))
+    // propagate status of commits before the last one over to the last commit's status,
+    // so that all statuses are (indirectly) considered by github when coloring the merge button green/red
+    private def propagateEarlierStati(pull: PullRequest) = {
+      import CommitStatus._
+      for {
+        commits       <- githubApi.pullRequestCommits(pr)
+        earlierStati  <- Future.sequence(commits.init.map(c => githubApi.commitStatus(c.sha)))
+        failingCommits = earlierStati.filterNot(_.success) // pending/failure
+      } yield {
+        (if (failingCommits.isEmpty) {
+          // override any prior status in the COMBINED context
+          // the last commit's status doesn't matter -- it'll be considered directly by github
+          (SUCCESS, "All previous commits successful.")
+        } else {
+          val worstState = if (failingCommits.exists(_.failure)) FAILURE else PENDING
+          (worstState, s"Found earlier commit(s) marked $worstState: ${failingCommits.map(_.sha.take(6)).mkString(", ")}")
+        }) match { case (state, msg) =>
+          githubApi.postStatus(commits.last.sha, CommitStatus(state, Some(COMBINED), Some(msg)))
+        }
       }
     }
 
-    // for all commits with pending status, or without status entirely, ensure that a jenkins job has been started
-    private def buildCommitsIfNeeded(pull: PullRequest) =
-      (for {
-        commits <- githubApi.pullRequestCommits(pr)
-      } yield {
-        for(commit <- commits) yield {
-          log.info(s"Build commit? $commit")
+    private def relevantMostRecentBuild(url: Option[String], sha: String, ignoreBuildsBefore: Option[Int]): Future[BuildStatus] = {
+      def relevant(bs: BuildStatus) = {
+        val expected = Map(
+          PARAM_PR        -> pr.toString,
+          PARAM_REPO_USER -> config.github.user,
+          PARAM_REPO_NAME -> config.github.repo,
+          PARAM_REPO_REF  -> sha)
 
-          for {
-            status <- githubApi.commitStatus(commit.sha)
-            _ = log.info(s"Status: $status")
-            if status.state == "pending"
-          } yield {
-            if (status.total_count == 0 || status.statuses.headOption.flatMap(_.target_url).isEmpty) launchBuild(commit.sha)
-            else jenkinsApi.buildStatus(status.statuses.head.target_url.get).onFailure{ case _ => launchBuild(commit.sha) }
-          }
-        }
-      }) onFailure {
-        case e => log.info(s"buildCommitsIfNeeded failed: $e");
+        bs.paramsMatch(expected) && ignoreBuildsBefore.forall(bs.number >= _)
       }
+
+      // both futures either fail or yield the most recent relevant status
+      val paramsMatchAtUrl = Future { url.get }.flatMap(url => jenkinsApi.buildStatus(url).filter(relevant))
+      val hasMatchingBuild = jenkinsApi.buildStatusesForJob(config.jenkins.job).map(_.find(relevant).get)
+
+      paramsMatchAtUrl fallbackTo hasMatchingBuild
+    }
+
+    /** make sure the commit has the expected status (updating it, to reflect what Jenkins told us)
+      * if no status found, fail --> we'll launch a build as a "fallback"
+      *
+      */
+    def ensureStatus(sha: String, url: Option[String], ignoreBuildsBefore: Option[Int], synchOnly: Boolean): Future[String] = (for {
+      mrb <- relevantMostRecentBuild(url, sha, ignoreBuildsBefore)
+    } yield {
+      // there was a build that was more recent than the force build command (if any)
+      if (url.contains(mrb.url)) "Relevant build found at $url (for ${sha.take(6)})"
+      else {
+        self ! mrb // the status we found on the PR didn't match what Jenkins told us --> synch while we're at it
+        "Synching status for ${sha.take(6)} based on ${mrb.url}."
+      }
+    }).recover{case _ if synchOnly => "Not building because synchOnly."}
+
+
+    // for all commits with pending status, or without status entirely, ensure that a jenkins job has been started
+    // if `ignoreBuildsBefore` is specified, jobs before it will be ignored (by job number)
+    private def buildCommitsIfNeeded(pull: PullRequest, ignoreBuildsBefore: Option[Int] = None, synchOnly: Boolean = false): Future[List[String]] =
+      for {
+        commits <- githubApi.pullRequestCommits(pr)
+        results <- Future.sequence(for(commit <- commits) yield {
+          log.debug(s"Build commit? $commit")
+          import CommitStatus._
+
+          val fetchCommitStatus = githubApi.commitStatus(commit.sha)
+          fetchCommitStatus.onFailure {  case e => log.info(s"Couldn't get status for ${commit.sha}: $e") }
+
+          (for {
+            status <- fetchCommitStatus.map { status =>
+              if (status.pending || ignoreBuildsBefore.nonEmpty) status
+              else throw new NoSuchElementException(s"No need to build: ${commit.sha} is ${status.state}")
+            }
+            jobUrl = status.statuses.headOption.flatMap(_.target_url)
+            buildRes <- ensureStatus(commit.sha, jobUrl, ignoreBuildsBefore, synchOnly) fallbackTo {
+              // we couldn't find the expected status --> launch build
+              launchBuild(commit.sha)
+            }
+          } yield buildRes).recover {
+            case e: NoSuchElementException => e.getMessage
+          }
+        })
+      } yield results
+
+    // onFailure { case e => log.info(s"Couldn't retrieve list of commits: $e") }
 
 //    def execCommands(pullRequest: PullRequest) = {
 //      val IGNORE_NOTE_TO_SELF = "(kitty-note-to-self: ignore "
